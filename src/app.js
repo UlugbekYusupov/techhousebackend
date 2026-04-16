fa4722cf1058e1a8c637aaed69830918e4fce6f1
UlugbekYusupov <bilmadim.uz@gmail.com>
2026-04-16 10:50:03 +0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// app.use("/", (req, res) => {
+//   res.json({ message: "API is running correctly!" });
+// });
+
+app.use("/api/auth", authRoutes);
+
+export default app;
